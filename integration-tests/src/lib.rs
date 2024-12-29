33154c46c9397f9ebdc69d50a07e1a6eb49b2319
Martin Raszyk <martin.raszyk@dfinity.org>
2024-12-29 15:44:57 +0100

    harden tests

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -227,6 +227,13 @@ fn test_set_member_block_cycles() {
         err.contains("The number of block cycles 100_000_000_001 is not a multiple of 1_000_000.")
     );
 
+    let member_cycles = get_member_cycles(&pic, admin).unwrap();
+    assert_eq!(member_cycles.block, 100_000_000_000_u128);
+
+    set_member_block_cycles(&pic, admin, 10_000_000_000_u128).unwrap();
+    let member_cycles = get_member_cycles(&pic, admin).unwrap();
+    assert_eq!(member_cycles.block, 10_000_000_000_u128);
+
     assert_eq!(pool_logs(&pic, admin).len(), 1);
     assert!(String::from_utf8(pool_logs(&pic, admin)[0].content.clone())
         .unwrap()
