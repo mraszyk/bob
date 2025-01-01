d27d942ff1ec530e7608b8bad01cff67ce657fb7
Martin Raszyk <martin.raszyk@dfinity.org>
2025-01-01 15:41:06 +0100

    simplify

--- pool/src/rewards.rs
+++ pool/src/rewards.rs
@@ -5,24 +5,20 @@ pub async fn pay_rewards(member: Principal) -> Result<(), String> {
     let _guard_principal = GuardPrincipal::new(member)
         .map_err(|guard_error| format!("Concurrency error: {:?}", guard_error))?;
     let mut rewards = get_member_rewards(member);
-    let mut err = None;
+    let mut res = Ok(());
     for reward in &mut rewards {
         if reward.bob_block_index.is_none() {
             match bob_transfer(member, reward.bob_reward).await {
                 Ok(block_idx) => {
                     reward.bob_block_index = Some(block_idx);
                 }
-                Err(e) => {
-                    err = Some(e);
+                Err(err) => {
+                    res = Err(err);
                     break;
                 }
             }
         }
     }
     set_member_rewards(member, rewards);
-    if let Some(e) = err {
-        Err(e)
-    } else {
-        Ok(())
-    }
+    res
 }
