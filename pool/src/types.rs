038c7951683408b35ff66a6316c7729b652b2177
Martin Raszyk <martin.raszyk@dfinity.org>
2025-01-01 15:38:39 +0100

    Revert "use orbit storable"
    This reverts commit e1996d00fe91e99e1c7f923a4e0e67e79feac821.

--- pool/src/types.rs
+++ pool/src/types.rs
@@ -1,16 +1,14 @@
 use candid::CandidType;
-use orbit_essentials_macros::storable;
+use serde::{Deserialize, Serialize};
 
-#[derive(CandidType, Debug, Default)]
-#[storable]
+#[derive(CandidType, Debug, Default, Serialize, Deserialize)]
 pub struct MemberCycles {
     pub block: u128,
     pub pending: u128,
     pub remaining: u128,
 }
 
-#[derive(CandidType, Clone, Debug)]
-#[storable]
+#[derive(CandidType, Clone, Debug, Serialize, Deserialize)]
 pub struct Reward {
     pub timestamp: u64,
     pub cycles_burnt: u128,
